22a9fe103bf47e8bcf7c2d3b1ba5307a0ecea3a6
lijianzhq <804390150@qq.coom>
2017-11-30 21:06:17 +0800

    重构logservice

--- src/Foundation/Foundation.LogService.Test/UnitTest1.cs
+++ src/Foundation/Foundation.LogService.Test/UnitTest1.cs
@@ -4,21 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-using DreamCube.Foundation.Log;
-
-namespace Foundation.Log.Test
+namespace DreamCube.Foundation.Log.Test
 {
     [TestClass]
     public class UnitTest1
     {
+        [DeploymentItem("Config", "Config")]
         [TestMethod]
         public void TestMethod1()
         {
-            DreamCube.Foundation.Log.Log.Root.LogDebug("LogDebug");
-            DreamCube.Foundation.Log.Log.Root.LogError("LogError");
-            DreamCube.Foundation.Log.Log.Root.LogFatal("LogFatal");
-            DreamCube.Foundation.Log.Log.Root.LogInfo("LogInfo");
-            DreamCube.Foundation.Log.Log.Root.LogWarn("LogWarn");
+            DreamCube.Foundation.LogService.Log.Root.LogDebug("LogDebug");
+            DreamCube.Foundation.LogService.Log.Root.LogInfo("LogInfo");
+            DreamCube.Foundation.LogService.Log.Root.LogWarn("LogWarn");
+            DreamCube.Foundation.LogService.Log.Root.LogError("LogError");
+            DreamCube.Foundation.LogService.Log.Root.LogFatal("LogFatal");
         }
     }
 }

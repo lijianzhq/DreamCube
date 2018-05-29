a93194e4fa9a9657b3636a9bb8da41cd083dbcb5
jianl <804390150@qq.com>
2018-05-29 23:02:56 +0800

    新增几个工具方法

--- src_new/Foundation/Mini.Foundation.Basic.Test/MyStringTest.cs
+++ src_new/Foundation/Mini.Foundation.Basic.Test/MyStringTest.cs
@@ -13,10 +13,17 @@ namespace Mini.Foundation.Basic.Test
     public class MyStringTest
     {
         [Test]
-        public void TestMethod()
+        public void TestMethod_GetBetweenStr1()
         {
             String str = MyString.GetBetweenStr("2010央视主打热播剧《血色沉香》全34集[国语字幕]", "《", "》");
             Assert.IsTrue(String.Equals(str, "血色沉香"));
+        }
+
+        [Test]
+        public void TestMethod_GetBetweenStr()
+        {
+            String str = MyString.GetBetweenStr("◎IMDB评分 <span style=\"COLOR: red\"><strong>8.5/10 (255,815 votes) Top 250: #46</strong></span>", "<strong>", "</strong>");
+            Assert.IsTrue(String.Equals(str, "8.5/10 (255,815 votes) Top 250: #46"));
         }
     }
 }
--- src_new/Foundation/Mini.Foundation.Basic/Utility/MyString.cs
+++ src_new/Foundation/Mini.Foundation.Basic/Utility/MyString.cs
@@ -10,8 +10,13 @@ namespace Mini.Foundation.Basic.Utility
     public static class MyString
     {
 
+#if NET20
         public static String GetBetweenStr(String value, String startChar, String endChar)
+#else
+        public static String GetBetweenStr(this String value, String startChar, String endChar)
+#endif
         {
+            if (IsInvisibleString(value)) return String.Empty;
             Regex rg = new Regex("(?<=(" + startChar + "))[.\\s\\S]*?(?=(" + endChar + "))", RegexOptions.Multiline | RegexOptions.Singleline);
             return rg.Match(value).Value;
         }
@@ -28,6 +33,117 @@ namespace Mini.Foundation.Basic.Utility
 #else
             return String.IsNullOrWhiteSpace(value);
 #endif
+        }
+
+        /// <summary>
+        /// 取标志字符的右边部分（从最后开始匹配标志字符）
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="value">标志字符</param>
+        /// <param name="bIgnoreCase">是否忽略大小写，默认为false，也就是默认是大小写敏感的</param>
+        /// <param name="defaultValue">当target为NULL或者空串时，返回的默认值</param>
+        /// <returns></returns>
+#if NET20
+        public static String RightOfLast(String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#else
+        public static String RightOfLast(this String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#endif
+        {
+            if (String.IsNullOrEmpty(target)) return defaultValue;
+            Int32 index = target.LastIndexOf(value,
+                ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+            return index >= 0 ? target.Substring(index + value.Length) : defaultValue;
+        }
+
+        /// <summary>
+        /// 取标志字符的左边部分（从最后开始匹配标志字符）
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="value">标志字符</param>
+        /// <param name="bIgnoreCase">是否忽略大小写，默认为false，也就是默认是大小写敏感的</param>
+        /// <returns></returns>
+#if NET20
+        public static String LeftOfLast(String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#else
+        public static String LeftOfLast(this String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#endif
+        {
+            if (String.IsNullOrEmpty(target)) return defaultValue;
+            Int32 index = target.LastIndexOf(value,
+                ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+            return index >= 0 ? target.Substring(0, index) : defaultValue;
+        }
+
+        /// <summary>
+        /// 取标志字符的左边部分
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="value">标志字符</param>
+        /// <param name="bIgnoreCase">是否忽略大小写，默认为false，也就是默认是大小写敏感的</param>
+        /// <returns></returns>
+#if NET20
+        public static String Left(String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#else
+        public static String Left(this String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#endif
+        {
+            if (String.IsNullOrEmpty(target)) return defaultValue;
+            Int32 index = target.IndexOf(value,
+                ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+            return index >= 0 ? target.Substring(0, index) : defaultValue;
+        }
+
+        /// <summary>
+        /// 获取左边Length长度的字符串
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+#if NET20
+        public static String Left(String target, Int32 length)
+#else
+        public static String Left(this String target, Int32 length)
+#endif
+        {
+            if (length <= 0) return "";
+            if (length >= target.Length) return target;
+            return target.Substring(0, length);
+        }
+
+        /// <summary>
+        /// 获取右边Length长度的字符串
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+#if NET20
+        public static String Right(String target, Int32 length)
+#else
+        public static String Right(this String target, Int32 length)
+#endif
+        {
+            if (length <= 0) return "";
+            if (length >= target.Length) return target;
+            return target.Substring(target.Length - length, length);
+        }
+
+        /// <summary>
+        /// 取标志字符的右边部分
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="value">标志字符</param>
+        /// <param name="bIgnoreCase">是否忽略大小写，默认为false，也就是默认是大小写敏感的</param>
+        /// <returns></returns>
+#if NET20
+        public static String Right(String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#else
+        public static String Right(this String target, String value, Boolean ignoreCase = false, String defaultValue = "")
+#endif
+        {
+            if (String.IsNullOrEmpty(target)) return defaultValue;
+            Int32 index = target.IndexOf(value,
+                ignoreCase ? StringComparison.CurrentCultureIgnoreCase : StringComparison.CurrentCulture);
+            return index >= 0 ? target.Substring(index + value.Length) : defaultValue;
         }
     }
 }

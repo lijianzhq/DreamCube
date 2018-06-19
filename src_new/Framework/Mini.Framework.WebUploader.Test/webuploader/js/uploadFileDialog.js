4b5ef41f5f168a77a1da975fa18dc378e2a3adb6
jianl <804390150@qq.com>
2018-06-20 01:10:17 +0800

    新增上传附件的插件代码

---
+++ src_new/Framework/Mini.Framework.WebUploader.Test/webuploader/js/uploadFileDialog.js
@@ -0,0 +1,303 @@
+﻿(function ($) {
+    // 当domReady的时候开始初始化
+    $(function () {
+        var uploader,// WebUploader实例
+            files = [],
+            GUID = WebUploader.Base.guid(); //当前页面是生成的GUID作为标示
+        // 实例化
+        uploader = WebUploader.create({
+            pick: {
+                id: '#selectFileBtn',
+                label: '点击选择文件'
+            },
+            formData: {
+                uid: 123
+            },
+            swf: 'webuploader0.1.5/Uploader.swf',
+            chunked: true, //分片处理大文件
+            chunkSize: 2 * 1024 * 1024,
+            server: 'FileSave.ashx',
+            disableGlobalDnd: true,
+            threads: 1, //上传并发数
+            //由于Http的无状态特征，在往服务器发送数据过程传递一个进入当前页面是生成的GUID作为标示
+            formData: { guid: GUID },
+            fileNumLimit: 100,  //上传文件的数量限制
+            compress: false, //图片在上传前不进行压缩
+            fileSizeLimit: 6 * 1024 * 1024 * 1024,    // 所有文件总大小限制 6G
+            fileSingleSizeLimit: 5 * 1024 * 1024 * 1024    // 单个文件大小限制 5 G
+        });
+
+        $("#tb_fileList").datagrid({
+            rownumbers: true,
+            columns: [[
+                { field: 'f_name', title: '文件名', width: 100, align: 'center' },
+                { field: 'f_size', title: '文件大小', width: 100, align: 'center' },
+                { field: 'f_status', title: '状态', width: 100, align: 'center' },
+                { field: 'f_op', title: '状态', width: 100, align: 'center' }
+            ]]
+        });
+
+        uploader.on('dialogOpen', function () {
+            console.log('here');
+        });
+
+        uploader.on('ready', function () {
+            window.uploader = uploader;
+        });
+
+        // 当有文件添加进来时执行，负责view的创建
+        function addFile(file) {
+            files.push({
+                "f_name": file.name
+            });
+            $("#tb_fileList").datagrid({ data: files });
+
+            var showError = function (code) {
+                switch (code) {
+                    case 'exceed_size':
+                        text = '文件大小超出';
+                        break;
+                    case 'interrupt':
+                        text = '上传暂停';
+                        break;
+                    default:
+                        text = '上传失败，请重试';
+                        break;
+                }
+                console.log(code);
+                //$info.text(text).appendTo($li);
+            };
+
+            if (file.getStatus() === 'invalid') {
+                showError(file.statusText);
+            }
+            else {
+                percentages[file.id] = [file.size, 0];
+                file.rotation = 0;
+            }
+
+            file.on('statuschange', function (cur, prev) {
+                if (prev === 'progress') {
+                    $prgress.hide().width(0);
+                } else if (prev === 'queued') {
+                    $li.off('mouseenter mouseleave');
+                    $btns.remove();
+                }
+
+                // 成功
+                if (cur === 'error' || cur === 'invalid') {
+                    console.log(file.statusText);
+                    showError(file.statusText);
+                    percentages[file.id][1] = 1;
+                } else if (cur === 'interrupt') {
+                    showError('interrupt');
+                } else if (cur === 'queued') {
+                    $info.remove();
+                    $prgress.css('display', 'block');
+                    percentages[file.id][1] = 0;
+                } else if (cur === 'progress') {
+                    $info.remove();
+                    $prgress.css('display', 'block');
+                } else if (cur === 'complete') {
+                    $prgress.hide().width(0);
+                    $li.append('<span class="success"></span>');
+                }
+                $li.removeClass('state-' + prev).addClass('state-' + cur);
+            });
+        }
+
+        // 负责view的销毁
+        function removeFile(file) {
+            var $li = $('#' + file.id);
+
+            delete percentages[file.id];
+            updateTotalProgress();
+            $li.off().find('.file-panel').off().end().remove();
+        }
+
+        function updateTotalProgress() {
+            var loaded = 0,
+                total = 0,
+                spans = $progress.children(),
+                percent;
+
+            $.each(percentages, function (k, v) {
+                total += v[0];
+                loaded += v[0] * v[1];
+            });
+
+            percent = total ? loaded / total : 0;
+
+
+            spans.eq(0).text(Math.round(percent * 100) + '%');
+            spans.eq(1).css('width', Math.round(percent * 100) + '%');
+            updateStatus();
+        }
+
+        function setState(val) {
+            var file, stats;
+
+            if (val === state) {
+                return;
+            }
+
+            $upload.removeClass('state-' + state);
+            $upload.addClass('state-' + val);
+            state = val;
+
+            switch (state) {
+                case 'pedding':
+                    $placeHolder.removeClass('element-invisible');
+                    $queue.hide();
+                    $statusBar.addClass('element-invisible');
+                    uploader.refresh();
+                    break;
+
+                case 'ready':
+                    $placeHolder.addClass('element-invisible');
+                    $('#filePicker2').removeClass('element-invisible');
+                    $queue.show();
+                    $statusBar.removeClass('element-invisible');
+                    uploader.refresh();
+                    break;
+
+                case 'uploading':
+                    $('#filePicker2').addClass('element-invisible');
+                    $progress.show();
+                    $upload.text('暂停上传');
+                    break;
+
+                case 'paused':
+                    $progress.show();
+                    $upload.text('继续上传');
+                    break;
+
+                case 'confirm':
+                    $progress.hide();
+                    $('#filePicker2').removeClass('element-invisible');
+                    $upload.text('开始上传');
+
+                    stats = uploader.getStats();
+                    if (stats.successNum && !stats.uploadFailNum) {
+                        setState('finish');
+                        return;
+                    }
+                    break;
+                case 'finish':
+                    stats = uploader.getStats();
+                    if (stats.successNum) {
+                        alert('上传完成');
+                    } else {
+                        // 没有成功的图片，重设
+                        state = 'done';
+                        location.reload();
+                    }
+                    break;
+            }
+            updateStatus();
+        }
+
+        uploader.onUploadProgress = function (file, percentage) {
+            var $li = $('#' + file.id),
+                $percent = $li.find('.progress span');
+
+            $percent.css('width', percentage * 100 + '%');
+            percentages[file.id][1] = percentage;
+            updateTotalProgress();
+        };
+
+        uploader.onFileQueued = function (file) {
+            fileCount++;
+            fileSize += file.size;
+
+            if (fileCount === 1) {
+                $placeHolder.addClass('element-invisible');
+                $statusBar.show();
+            }
+            addFile(file);
+            setState('ready');
+            updateTotalProgress();
+        };
+
+        uploader.onFileDequeued = function (file) {
+            fileCount--;
+            fileSize -= file.size;
+
+            if (!fileCount) {
+                setState('pedding');
+            }
+            removeFile(file);
+            updateTotalProgress();
+        };
+
+        //all算是一个总监听器
+        uploader.on('all', function (type, arg1, arg2) {
+            console.log("all监听：", type, arg1, arg2);
+            var stats;
+            switch (type) {
+                case 'uploadFinished':
+                    setState('confirm');
+                    break;
+                case 'startUpload':
+                    setState('uploading');
+                    break;
+                case 'stopUpload':
+                    setState('paused');
+                    break;
+            }
+        });
+
+        // 文件上传成功,合并文件。
+        uploader.on('uploadSuccess', function (file, response) {
+            if (response.Chunked) {
+                jQuery.ajax({
+                    url: "MergeFile.ashx",
+                    type: "post",
+                    data: { guid: GUID, id: file.id, fileName: file.name },
+                    dataType: "json",
+                    success: function (msg) {
+                        alert(msg);
+                    },
+                    error: function (XMLHttpRequest, textStatus, errorThrown) {
+                        alert("XMLHttpRequest.status:" + XMLHttpRequest.status);
+                        alert("XMLHttpRequest.readyState:" + XMLHttpRequest.readyState);
+                        alert("XMLHttpRequest.textStatus:" + textStatus);
+                    },
+                    complete: function (XMLHttpRequest, textStatus) {
+                        this; // 调用本次AJAX请求时传递的options参数
+                    }
+                });
+            }
+        });
+
+        uploader.onError = function (code) {
+            alert('Eroor: ' + code);
+        };
+
+        $upload.on('click', function () {
+            if ($(this).hasClass('disabled')) {
+                return false;
+            }
+
+            if (state === 'ready') {
+                uploader.upload();
+            } else if (state === 'paused') {
+                uploader.upload();
+            } else if (state === 'uploading') {
+                uploader.stop();
+            }
+        });
+
+        $info.on('click', '.retry', function () {
+            uploader.retry();
+        });
+
+        $info.on('click', '.ignore', function () {
+            alert('todo');
+        });
+
+        $upload.addClass('state-' + state);
+        updateTotalProgress();
+    });
+
+})(jQuery);
---
+++ src_new/Framework/Mini.Framework.WebUploader.Test/webuploader/js/webuploaderbar.js
@@ -0,0 +1,118 @@
+﻿(function ($, w, udf) {
+    function webuploaderbar() {
+        var me = this;
+
+        /**
+         * 获取配置的方法
+         * */
+        this.configs = function () {
+            var config = {
+                index: 0,//当前的barindex（一个页面可以有多个这种bar）
+                chunked: true, //分片处理大文件
+                chunkSize: 2 * 1024 * 1024,
+                server: 'FileSave.ashx', //接收文件数据的页面
+                merge: 'MergeFile.ashx', //合并文件的页面
+                disableGlobalDnd: true,
+                threads: 1, //上传并发数
+                //由于Http的无状态特征，在往服务器发送数据过程传递一个进入当前页面是生成的GUID作为标示
+                //formData: { guid: GUID },
+                fileNumLimit: 100,  //上传文件的数量限制
+                compress: false, //图片在上传前不进行压缩
+                fileSizeLimit: 6 * 1024 * 1024 * 1024,    // 所有文件总大小限制 6G
+                fileSingleSizeLimit: 5 * 1024 * 1024 * 1024,    // 单个文件大小限制 5 G
+                templateid: "webuploaderbar_htmltemplate",
+                swf: me.getFolderUrl() + 'webuploader0.1.5/Uploader.swf',
+            };
+            var config2 = {
+                pick: {
+                    id: '#' + config.templateid + "_" + config.index,
+                    label: '＋'
+                },
+                barHtml: "<div data-bind=\"template: '" + config.templateid + "'\"></div>"
+            };
+            return $.extend(config, config2);
+        };
+
+        /**
+        * 获取本插件所在的web的目录url
+        * */
+        this.getFolderUrl = function () {
+            var scripts = document.scripts;
+            for (var i = 0; i < scripts.length; i++) {
+                var js = scripts[i];
+                var index = js.src.lastIndexOf('/webuploader/js/webuploaderbar.js');
+                if (index > 0) {
+                    return js.src.substr(0, index) + "/webuploader/";
+                }
+            }
+            console.log("folder name[webuploader] or file name [webuploaderbar.js] has been changed!");
+            return "";
+        };
+
+        /**
+         * 获取html模板文件的url
+         * */
+        this.getHtmlTemplateFileUrl = function () {
+            var sFolderUrl = me.getFolderUrl();
+            return sFolderUrl + "html/bartemplate.html";
+        };
+
+        function Uploader() {
+            var parent = me;
+            this.create = function (configs) {
+                // 实例化
+                var uploader = WebUploader.create(configs);
+                uploader.vm = {
+                    files: []
+                };//viewmodel
+                uploader.onFileQueued = function (file) {
+                    var it = uploader;
+                    it.vm.files.push(file);
+                };
+                return uploader;
+            }
+        };
+        this.uploader = new Uploader();
+    };
+
+    function viewModel() {
+        this.addFile = function () {
+            layer.open({
+                type: 2,
+                title: '文件上传',
+                shadeClose: true,
+                shade: 0.2,
+                area: ['50%', '50%'],
+                content: bar.getFolderUrl() + 'html/uploadFileDialog.html'
+            });
+            return false;
+        }
+    };
+
+    var bar = new webuploaderbar();
+    var barVM = [];
+
+    $.fn.extend({
+        "makewebuploaderbar": function (config) {
+            var configs = $.extend(config, bar.configs(), { index: barVM.length });
+            if ($("#" + configs.templateid).length == 0) {
+                var me = $(this);
+                $.get(bar.getHtmlTemplateFileUrl())
+                    .done(function (responseText) {
+                        responseText = responseText.replace(/{templateid}/gi, configs.templateid)
+                            .replace(/{addfilebtnid}/gi, configs.pick.id.substr(1));
+                        me.append(responseText);
+                        me.append(configs.barHtml);
+                        ko.applyBindings(new viewModel());
+
+                        //var uploader = bar.uploader.create(configs);
+                        //barVM.push(ko.observable(new viewModel()));
+                    });
+            }
+            else {
+                $(this).append(barHtml);
+                ko.applyBindings(barVM);
+            }
+        }
+    });
+})(jQuery, window, undefined);
